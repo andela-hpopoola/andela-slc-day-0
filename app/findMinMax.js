b49d483aef9ac7b7652292d1b1eb00b484430042
Haruna Popoola <haruna.popoola@yahoo.com>
2017-03-11 10:41:43 +0100

    Added findMinMax Function

--- app/findMinMax.js
+++ app/findMinMax.js
@@ -2,9 +2,34 @@
 
 // Find the Minimum and Maximum in an array of number
 var findMinMax = function(arr) {
-	
+	// Assume first element as largest and smallest
+	var largest = arr[0], smallest = arr[0];
+
+	// Get total elements in the array
+	var total_elements = arr.length;
+
+	// Return empty array if array is empty or not a number
+	if ((typeof largest !== 'undefined') && (typeof largest !== 'number')) {
+		return [];
+	}
+
+	// Find the smallest and largest
+	for (var i = 1; i < arr.length; i++){
+		if (arr[i] > largest)
+			largest = arr[i];
+		if (arr[i] < smallest)
+			smallest = arr[i]
+	}
+
+	// Return a single value if largest === smallest
+	if (largest === smallest) {
+		return [arr[0]];
+	}
+	// Return [smallest, largest]
+	else return [smallest, largest];
 }
 
+// console.log(findMinMax([1,1,1,1,1]));
 
 module.exports = {
 	findMinMax: findMinMax
--- spec/minmax/MinMaxSpec.js
+++ spec/minmax/MinMaxSpec.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var app = require('../../app/playground.js');
+var app = require('../../app/findMinMax.js');
 
 describe('Min-Max Numbers in a List: ', function () {
 
@@ -10,8 +10,20 @@ describe('Min-Max Numbers in a List: ', function () {
         expect(app.findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
       });
 
+      it('should return [1,4] for [4, 3, 2, 1]', function () {
+        expect(app.findMinMax([4, 3, 2, 1])).toEqual([1, 4]);
+      });
+
       it('should return [4, 6] for [6, 4]', function () {
         expect(app.findMinMax([6, 4])).toEqual([4, 6]);
+      });
+
+      it('should return [4, 6] for [4, 6]', function () {
+        expect(app.findMinMax([4, 6])).toEqual([4, 6]);
+      });
+
+      it('should return [-10, -1] for [-10, -5 , -3, -1 ]', function () {
+        expect(app.findMinMax([-10, -5 , -3, -1 ])).toEqual([-10, -1]);
       });
 
       it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
@@ -24,6 +36,23 @@ describe('Min-Max Numbers in a List: ', function () {
 
       it('should return [4] for [4, 4, 4, 4]', function () {
         expect(app.findMinMax([4, 4, 4, 4])).toEqual([4]);
+      });
+
+      it('should return [1] for [1, 1, 1, 1, 1, 1, 1]', function () {
+        expect(app.findMinMax([1, 1, 1, 1, 1, 1, 1])).toEqual([1]);
+      });
+
+      it('should return [3] for [3, 3, 3]', function () {
+        expect(app.findMinMax([3, 3, 3])).toEqual([3]);
+      });
+
+    });
+
+
+    describe('Return empty array when the list is not an integer', function () {
+
+      it('should return empty array for ["a","n","d","e","l","a"]', function () {
+        expect(app.findMinMax(["a","n","d","e","l","a"])).toEqual([]);
       });
 
     });
